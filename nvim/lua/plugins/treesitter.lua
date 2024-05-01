de89810196da5edb52434d5437a4837dfa0c2159
Nikita Patskov <patskovn@yahoo.com>
2024-05-01 09:23:34 +0100

    Initial Commit

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "elixir",
+        "heex",
+        "javascript",
+        "html",
+        "go",
+        "rust",
+        "swift",
+      },
+      auto_install = true,
+      sync_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+}
