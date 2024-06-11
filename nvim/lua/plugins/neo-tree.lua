53347cdd061dd8dc7fac367f96d58bf37b579d38
Nikita Patskov <patskovn@yahoo.com>
2024-06-11 20:49:03 +0100

    Update

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -9,6 +9,24 @@ return {
 		},
 		config = function()
 			vim.keymap.set("n", "<C-j>", ":Neotree filesystem reveal left<CR>", { noremap = true, silent = true })
+
+			require("neo-tree").setup({
+				filesystem = {
+					window = {
+						mappings = {
+							["<leader>p"] = "image_wezterm", -- " or another map
+						},
+					},
+					commands = {
+						image_wezterm = function(state)
+							local node = state.tree:get_node()
+							if node.type == "file" then
+								require("image_preview").PreviewImage(node.path)
+							end
+						end,
+					},
+				},
+			})
 		end,
 	},
 	{ "kyazdani42/nvim-web-devicons" },
